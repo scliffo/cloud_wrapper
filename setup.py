0181db672027fa8292fe537c6a9abf4d3362e922
kblaj41 <kblaj41@gmail.com>
2020-02-24 16:50:22 +0000

    Updated license and readme

--- setup.py
+++ setup.py
@@ -14,8 +14,23 @@ setup(
     long_description=readme,
     author='kblaj41',
     author_email='kblaj41@gmail.com',
-    url='https://pypi.org/project/cloud_wrapper/',
+    url='https://github.com/kblaj41/cloud_wrapper',
     license=license,
     include_package_data=True,
-    packages=find_packages(exclude=('tests', 'docs'))
+    packages=find_packages(exclude=('tests', 'docs')),
+    install_requires=[
+          'boto3',
+          'botocore',
+          'jsonmerge',
+          'python-dateutil',
+          'numpyencoder',
+      ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+        'Intended Audience :: Developers',      # Define that your audience are developers
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',   # Again, pick a license
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+  ]
 )
